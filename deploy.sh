af59749287034f855759b67bda0699ea70c1a492
mybeliefs <mark.mybeliefs@gmail.com>
2022-04-18 10:32:07 -0700

    initial setup

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+GIT_USER=mybeliefs \
+  CURRENT_BRANCH=main \
+  DEPLOY_BRANCH=docsite-pages
+  yarn deploy
---
+++ docusaurus.config.js
@@ -0,0 +1,92 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'My Beliefs Site',
+  tagline: 'Why I believe',
+  url: 'https://mybeliefs.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'mybeliefs', 
+  projectName: 'mybeliefs.github.io', 
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          // Please change this to your repo.
+          editUrl: 'https://github.com/mybeliefs.github.io/edit/main/',
+        },
+        blog: false, 
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'right',
+            label: 'Thoughts',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Thoughts',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Whatever',
+            items: [
+              {
+                label: 'Site',
+                href: 'https://mybeliefs.github.io/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/mybeliefs/mybeliefs.github.io',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Beliefs project. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
